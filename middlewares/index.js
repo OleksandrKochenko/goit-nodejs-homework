b4497194187325adb3730c70c44811cb2b901168
OleksandrKochenko <121250212+OleksandrKochenko@users.noreply.github.com>
2023-06-20 21:11:13 +0300

    upload avatar, rename it , update URL

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,10 +2,12 @@ const handleMongooseError = require("./handleMonguseError");
 const isValidId = require("./isValidId");
 const validateData = require("./validateData");
 const validatePatchData = require("./validatePatchData");
+const upload = require("./upload");
 
 module.exports = {
   handleMongooseError,
   isValidId,
   validateData,
   validatePatchData,
+  upload,
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,11 +6,13 @@ const {
   getCurrentUser,
   logout,
   updateSubscription,
+  updateAvatar,
 } = require("./api");
 const validateUserRegister = require("../../middlewares/validateUserRegister");
 const validateUserLogIn = require("../../middlewares/validateUserLogIn");
 const authentificate = require("../../middlewares/authentificate");
 const validateSubscriptionData = require("../../middlewares/validateSubscriptionData");
+const { upload } = require("../../middlewares");
 
 const router = express.Router();
 
@@ -23,5 +25,7 @@ router.get("/current", authentificate, getCurrentUser);
 router.post("/logout", authentificate, logout);
 
 router.patch("/", authentificate, validateSubscriptionData, updateSubscription);
+
+router.patch("/avatars", authentificate, upload.single("avatar"), updateAvatar); // upload.array('avatar', 8) // upload.fields([{name: 'avatar', maxCount: 2}])
 
 module.exports = router;
